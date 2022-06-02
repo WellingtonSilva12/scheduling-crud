ba8fef3cdaf1676fd1447c07b1645663a8f40a01
WellingtonSilva12 <wellingtonsilva180g@gmail.com>
2022-06-02 16:06:27 -0300

    Crud com local storage

--- CRUD 02/script.js
+++ CRUD 02/script.js
@@ -8,8 +8,19 @@ var closeModal = function () {
   document.getElementById('modal').classList.remove('active')
 }
 
+//Eventos
+
 document.getElementById('registerActivity').addEventListener('click', openModal)
 
 document.getElementById('modalClose').addEventListener('click', closeModal)
 
 document.getElementById('modalCancel').addEventListener('click', closeModal)
+
+// CRUD
+function readFormData() {
+  var formData = {}
+  formData['nameTask'] = document.getElementById('nameTask').value
+  formData['descriptionTask'] = document.getElementById('descriptionTask').value
+
+  return formData
+}
